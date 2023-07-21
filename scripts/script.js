5b5ace9c877f4bbac6afc10fc458649e6139057b
VanehSann <vanessacbsantos22@gmail.com>
2023-07-21 09:37:27 -0300

    feat: add content, div sections, curiculum, and more..

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+const directLinks = document.getElementById("direct-links");
+const writtenInfo = document.getElementById("written-info");
+const tabDirectLinks = document.querySelector(".tab-btn-direct-links");
+const tabWrittenInfo = document.querySelector(".tab-btn-written-info");
+
+function showDirectLinks() {
+    directLinks.classList.add("show");
+    writtenInfo.classList.remove("show");
+    tabDirectLinks.classList.add("active");
+    tabWrittenInfo.classList.remove("active");
+}
+
+function showWrittenInfo() {
+    writtenInfo.classList.add("show");
+    directLinks.classList.remove("show");
+    tabWrittenInfo.classList.add("active");
+    tabDirectLinks.classList.remove("active");
+}
